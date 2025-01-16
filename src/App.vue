4134ba1ff3eeb5c374b93c3b95d94aaec73f6d12
MRoyhanF <froyhan0@gmail.com>
2025-01-16 15:45:52 +0700

    meeting:30 | part 4

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <home-page></home-page>
+  </div>
+</template>
+
+<script>
+import HomePage from "./components/pages/HomePage.vue";
+export default {
+  components: {
+    HomePage,
+  },
+};
+</script>
---
+++ src/components/pages/HomePage.vue
@@ -0,0 +1,18 @@
+<script>
+import RecipeList from "../recipe/RecipeList.vue";
+export default {
+  components: {
+    RecipeList,
+  },
+};
+</script>
+
+<template>
+  <div class="container-md my-5 py-5">
+    <div class="recipe__title text-center">
+      <h2>All Recipe</h2>
+      <p style="font-size: 15px">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius magni voluptates cumque accusantium labore maiores, itaque saepe rerum eos dicta?</p>
+    </div>
+    <recipe-list></recipe-list>
+  </div>
+</template>
